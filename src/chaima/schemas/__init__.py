05684fdf8d7215cd9d0f8b3167cb5f121e800855
Fabian Zills <fzills@icp.uni-stuttgart.de>
2026-04-10 12:55:14 +0200

    feat: add PaginatedResponse generic schema

--- src/chaima/schemas/__init__.py
+++ src/chaima/schemas/__init__.py
@@ -1,6 +1,8 @@
+from chaima.schemas.pagination import PaginatedResponse
 from chaima.schemas.user import UserCreate, UserRead, UserUpdate
 
 __all__ = [
+    "PaginatedResponse",
     "UserCreate",
     "UserRead",
     "UserUpdate",
